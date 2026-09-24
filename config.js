8bf9faa4860e91c149d8a6a288aa3f9fefc3c44c
Prince Akakpo <prince.akakpo@icloud.com>
2026-09-24 01:12:23 +0000

    cleaning up the repo

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  node_env: process.env.NODE_ENV || "development",
-  server_port: process.env.PORT || 3000,
-};
-
-export default config;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import config from "./config.js";
+import config from "./config/config.js";
 import connectDB from "./config/connectDB.js";
 
 await connectDB();
